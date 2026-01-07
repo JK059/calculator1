a2fcc7a2f5d09bc0d8eda76534ea0fb2261c78f1
jayakumaran <jaium006@gmail.com>
2026-01-07 19:01:35 +0530

    Initial commit:Calculator program

---
+++ calculator.py
@@ -0,0 +1,27 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Choose an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+print("5. Integer Division (//)")
+choice = input("Enter your choice (1/2/3/4/5): ")
+if choice == "1":
+    print("Result:", num1 + num2)
+elif choice == "2":
+    print("Result:", num1 - num2)
+elif choice == "3":
+    print("Result:", num1 * num2)
+elif choice == "4":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed")
+elif choice == "5":
+    if num2 != 0:
+        print("Result:", num1 // num2)
+    else:
+        print("Error: Division by zero is not allowed")
+else:
+    print("Invalid choice")
